a4972ea60e98a76546df294f1c0c6bd95e192bb3
BoboBananas <bhvrddy@gmail.com>
2018-01-21 04:15:29 -0800

    Create trends.py
    Link: https://github.com/pat310/google-trends-api/wiki/Google-Trends-Categories

---
+++ trends.py
@@ -0,0 +1,8 @@
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+kw_list = ['Shirt']
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+print pytrends.related_queries()
