9311e4e720487567b84cfc24cb3e3f6ff9bdd8bc
Rohan Koodli <rovik05@gmail.com>
2018-01-20 16:08:18 -0800

    Add test links

---
+++ amazontrends.py
@@ -0,0 +1,59 @@
+
+img = []
+link = []
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/51KlaoGaVKL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Womens-Paris-Bohemian-Sleeve/dp/B011SP0IUC/ref=sr_1_1?s=apparel&ie=UTF8&qid=1516488999&sr=1-1&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/410z-V3xCzL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Sleeve-Casual-Cocktail-Floral/dp/B01N1A38VG/ref=sr_1_2?s=apparel&ie=UTF8&qid=1516488999&sr=1-2&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/51T1sApy%2BhL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Womens-Sleeve-V-Neck-Bohemian/dp/B073XV9572/ref=sr_1_3?s=apparel&ie=UTF8&qid=1516488999&sr=1-3&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41rANSPOK0L._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Sleeve-V-Neck-Floral-Bohemian/dp/B074KNRQDF/ref=sr_1_4?s=apparel&ie=UTF8&qid=1516488999&sr=1-4&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/51X6nOtmCVL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Paris-Bohemian-Sleeve-Floral/dp/B074KNLD4H/ref=sr_1_5?s=apparel&ie=UTF8&qid=1516488999&sr=1-5&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41sM7hiTKNL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Paris-Short-Sleeve-Dress/dp/B06Y1TCXBD/ref=sr_1_6?s=apparel&ie=UTF8&qid=1516488999&sr=1-6&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/417XYNm-5qL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Paris-Floral-Bohemian-Sleeve/dp/B00JHQKJQ8/ref=sr_1_7?s=apparel&ie=UTF8&qid=1516488999&sr=1-7&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41iLLezChpL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Seasons-Change-Sleeve-Damask/dp/B00FH7C5UY/ref=sr_1_8?s=apparel&ie=UTF8&qid=1516488999&sr=1-8&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/414V6Z0HGEL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Paris-Bohemian-Sleeve-Short/dp/B013S1XKRW/ref=sr_1_9?s=apparel&ie=UTF8&qid=1516488999&sr=1-9&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41bEIOo-9XL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Womens-Sleeve-T-Shirt-Contrast/dp/B00GXI1CIW/ref=sr_1_10?s=apparel&ie=UTF8&qid=1516488999&sr=1-10&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/514lz7VXTSL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Bohemian-Print-Sleeveless-Floral/dp/B072BBH62F/ref=sr_1_11?s=apparel&ie=UTF8&qid=1516488999&sr=1-11&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/51c3lf%2BaIZL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Trend-Bohemian-Print-Retro-Multicolor/dp/B01BS92BJ4/ref=sr_1_12?s=apparel&ie=UTF8&qid=1516488999&sr=1-12&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41inu9IkOEL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/Womens-Trend-Sleeveless-Halter-Medium/dp/B01BPU3I2U/ref=sr_1_15?s=apparel&ie=UTF8&qid=1516488999&sr=1-15&nodeID=7141123011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/31YaJuM04TL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/IZOD-Little-Toddler-Sleeve-Oxford/dp/B00APDFJP8/ref=lp_3002596011_1_1?s=apparel&ie=UTF8&qid=1516492655&sr=1-1&nodeID=3002596011&psd=1")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/41ACx4mOpaL._AC_UL260_SR200,260_.jpg")
+link.append("https://www.amazon.com/s/ref=lp_3002596011_blf_1_5?fst=fsl%3AMarvel&rh=i%3Afashion%2Cn%3A3002596011%2Cp_89%3AMarvel&ie=UTF8&qid=1516492655")
+
+img.append("https://images-na.ssl-images-amazon.com/images/I/91MvLKs2bLL._AC_SR201,266_.jpg")
+link.append("https://www.amazon.com/gp/product/B00JULWEAE/ref=s9_acsd_top_hd_bw_b4Mio_c_x_3_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-5&pf_rd_r=5J1Q7MVAEVFF2TMC4PKY&pf_rd_t=101&pf_rd_p=8a8f09ed-5cb5-5dc6-8bb3-60e637fbcb91&pf_rd_i=1040658")
+
+print(len(img))
+
+import pickle
+import os
+
+pickle.dump(img, open(os.getcwd()+'images.p', 'wb'))
+pickle.dump(link, open(os.getcwd()+'links.p', 'wb'))
